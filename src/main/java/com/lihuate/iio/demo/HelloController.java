f66830f9b3f749a7efb4c4ea32d6b1357fbd83d1
lihuate@gmail.com <wonuibi123>
2019-11-20 23:52:11 +0800

    第一次提交嗯？

---
+++ src/main/java/com/lihuate/iio/demo/HelloController.java
@@ -0,0 +1,12 @@
+package com.lihuate.iio.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/say")
+    String say(){
+        return "fast spring boot start";
+    }
+}
---
+++ src/test/java/com/lihuate/iio/demo/Mgr06.java
@@ -0,0 +1,35 @@
+package com.lihuate.iio.demo;
+
+public class Mgr06 {
+private static Mgr06 ADMIN;
+private Mgr06(){}
+public static Mgr06 getInstance(){
+    if (ADMIN == null) {
+        synchronized (Mgr06.class) {
+            if (ADMIN == null) {
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                ADMIN = new Mgr06();
+            }
+        }
+    }
+    return ADMIN;
+}
+public void m(){
+    System.out.println("m");
+}
+    public static void main(String[] args) {
+
+        for (int i = 0; i <100 ; i++) {
+            new Thread(()->{
+                System.out.println(Mgr06.getInstance().hashCode());
+            }).start();
+        }
+//        for (int i = 0; i <100 ; i++) {
+//            new Thread(Mgr06::run).start();
+//        }
+    }
+}
